e278c097b888bb47c126de8e340d73c6a8e0e4af
justin <justin.nethers@gmail.com>
2024-08-04 19:30:27 -0400

    Hide sealed and partial rolls.

--- resources/js/Pages/Filament/Index.vue
+++ resources/js/Pages/Filament/Index.vue
@@ -4,7 +4,7 @@ import NavLink from '@/Components/NavLink.vue';
 import {Head, usePage} from '@inertiajs/vue3';
 import FilamentRoll from "@/Components/FilamentRoll.vue";
 import RollInformation from "@/Pages/Filament/partials/RollInformation.vue";
-import {computed, nextTick, onBeforeUnmount, onMounted, ref} from "vue";
+import {computed, nextTick, onBeforeUnmount, onMounted, reactive, ref} from "vue";
 import {hasBeenUsed} from "@/utils.js";
 import InputLabel from "@/Components/InputLabel.vue";
 
@@ -13,6 +13,14 @@ const page = usePage();
 const filterBarVisible = ref(false);
 const filterBarHeight = ref(0);
 const filterBarRef = ref(null);
+const emptyRollsVisible = ref(false);
+const partialRollsVisible = ref(true);
+const sealedRollsVisible = ref(true);
+const isRollVisible = ref({
+    empty: true,
+    sealed: true,
+    partial: true,
+});
 
 const brandFilter = ref("");
 const filamentTypeFilter = ref("");
@@ -80,6 +88,39 @@ const fullRollsGroupedByColorAndBrand = computed(() => {
         return acc;
     }, {});
 });
+
+const toggleRollVisibility = (type) => {
+    if (isRollVisible.value[type]) {
+        isRollVisible.value[type] = false;
+    } else {
+        isRollVisible.value[type] = true;
+        nextTick(() => {
+            filamentRolls.value.filter(roll => {
+                return hasBeenUsed(roll) === isRollVisible.value[type];
+            }).forEach((roll, index) => {
+                setTimeout(() => {
+                    roll.visible = true;
+                }, index * 50);
+            });
+        });
+
+    }
+};
+
+const toggleEmptyRolls = () => {
+    if (emptyRollsVisible.value) {
+        emptyRollsVisible.value = false;
+    } else {
+        emptyRollsVisible.value = true;
+        nextTick(() => {
+            emptyRolls.value.forEach((roll, index) => {
+                setTimeout(() => {
+                    roll.visible = true;
+                }, index * 50);
+            });
+        });
+    }
+};
 
 const toggleFilterBar = () => {
     filterBarVisible.value = !filterBarVisible.value;
@@ -179,28 +220,41 @@ onBeforeUnmount(() => {
                 }"
             >
                 <div v-if="partialRolls.length">
-                    <h2 class="text-slate-50 mb-4">Partial Rolls <small class="font-light">(<span v-html="partialRolls.length"></span>)</small></h2>
-                    <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+                    <div class="flex justify-between items-center">
+                        <h2 class="text-slate-50 mb-4">Partial Rolls <small class="font-light">(<span v-html="partialRolls.length"></span>)</small></h2>
+
+                        <button
+                            class="p-2 bg-transparent text-slate-50 border border-slate-500"
+                            @click="toggleRollVisibility('partial')"
+                        >{{ isRollVisible['partial'] ? 'Hide Partial Rolls' : 'Show Partial Rolls'}}</button>
+                    </div>
+                    <transition-group name="empty-rolls" tag="ul" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
                         <li
-                            v-for="roll in partialRolls"
-                            class="flex gap-4 md:gap-6 bg-gray-300 p-6 rounded-md shadow-inner hover:shadow-2xl"
+                            v-for="roll in isRollVisible['partial'] ? partialRolls : []"
+                            class="flex gap-4 md:gap-6 bg-gray-300 p-6 rounded-md shadow-inner hover:shadow-2xl empty-roll-item"
                         >
                             <FilamentRoll
                                 :roll="roll"
                             />
                             <RollInformation text-size="text-3xl md:text-2xl" :roll="roll"/>
                         </li>
-                    </ul>
+                    </transition-group>
 
                     <br>
                 </div>
 
                 <div v-if="fullRolls.length">
-                    <h2 class="text-slate-50 mb-4">Sealed Rolls <small class="font-light">(<span v-html="fullRolls.length"></span>)</small></h2>
-                    <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+                    <div class="flex justify-between items-center">
+                        <h2 class="text-slate-50 mb-4">Sealed Rolls <small class="font-light">(<span v-html="fullRolls.length"></span>)</small></h2>
+                        <button
+                            class="p-2 bg-transparent text-slate-50 border border-slate-500"
+                            @click="toggleRollVisibility('sealed')"
+                        >{{ isRollVisible['sealed'] ? 'Hide Sealed Rolls' : 'Show Sealed Rolls'}}</button>
+                    </div>
+                    <transition-group name="empty-rolls" tag="ul" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-6">
                         <li
-                            v-for="color in fullRollsGroupedByColorAndBrand"
-                            class="grid grid-cols-1 px-3 pb-3 bg-gray-300 rounded-md shadow-inner"
+                            v-for="color in isRollVisible['sealed'] ? fullRollsGroupedByColorAndBrand : []"
+                            class="grid grid-cols-1 px-3 pb-3 bg-gray-300 rounded-md shadow-inner hover:shadow-2xl empty-roll-item"
                         >
                             <div v-if="color.length == 1">
                                 <div v-for="roll in color">
@@ -227,18 +281,27 @@ onBeforeUnmount(() => {
                                 </div>
                             </div>
                         </li>
-                    </ul>
+                    </transition-group>
 
                     <br>
                 </div>
 
 
                 <div v-if="emptyRolls.length">
-                    <h2 class="text-slate-50 mb-4">Empty Rolls <small class="font-light">(<span v-html="emptyRolls.length"></span>)</small></h2>
-                    <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-6">
+                    <div class="flex justify-between items-center">
+                        <h2 class="text-slate-50 mb-4">Empty Rolls <small class="font-light">(<span v-html="emptyRolls.length"></span>)</small></h2>
+
+                        <button
+                            class="p-2 bg-transparent text-slate-50 border border-slate-500"
+                            @click="toggleRollVisibility('empty')"
+                        >{{ isRollVisible['empty'] ? 'Hide Empty Rolls' : 'Show Empty Rolls'}}</button>
+
+                    </div>
+                    <transition-group name="empty-rolls" tag="ul" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-6">
                         <li
-                            v-for="roll in emptyRolls"
-                            class="grid grid-cols-1 px-3 pb-3 bg-gray-300 rounded-md shadow-inner hover:shadow-2xl"
+                            v-for="roll in isRollVisible['empty'] ? emptyRolls : []"
+                            :key="roll.id"
+                            class="grid grid-cols-1 px-3 pb-3 bg-gray-300 rounded-md shadow-inner hover:shadow-2xl empty-roll-item"
                         >
                             <FilamentRoll
                                 :roll="roll"
@@ -247,7 +310,7 @@ onBeforeUnmount(() => {
                             />
                             <RollInformation text-size="text-xl md:text-2xl" :show-remaining="false" :roll="roll" :is-empty="true" />
                         </li>
-                    </ul>
+                    </transition-group>
                 </div>
 
                 <div v-if="!emptyRolls.length && !fullRolls.length && !partialRolls.length">
@@ -272,6 +335,23 @@ onBeforeUnmount(() => {
 .slide-up-down-enter-to,
 .slide-up-down-leave-from {
     transform: translateY(0);
+}
+
+.empty-rolls-enter-active,
+.empty-rolls-leave-active {
+    transition: all 0.5s ease;
+}
+.empty-rolls-enter-from,
+.empty-rolls-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+}
+.empty-rolls-move {
+    transition: transform 0.5s ease;
+}
+
+.empty-roll-item {
+    transition: all 0.5s ease;
 }
 
 </style>
